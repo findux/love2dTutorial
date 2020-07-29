d6cf68a6ff286fd7f131b045a44d65a4065c1f52
eyup <devteam@adeko.com>
2020-07-29 14:59:57 +0300

    Bismillah

---
+++ main.lua
@@ -0,0 +1,13 @@
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    require("lldebugger").start()
+end 
+
+function love.update()
+  love.graphics.print("dddd")
+end
+function love.load()
+  whale = love.graphics.newImage("whale.png")
+end
+function love.draw()
+  love.graphics.draw(whale, 300, 200)
+end
